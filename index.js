4825d95f170f953428373384731cd97ec7434fbf
Steven Zapata <szapata427@gmail.com>
2018-08-11 05:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
+  var array = []; 
   for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i]);
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
+  }
+
+
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var count = 0
+  while(count < facts.length) {
+  array.push(facts[count] + '!!!'); 
+  count++;
+  }
+  return array
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function johnLennonFacts(facts) {
   var results = [];
   var count = 0;
